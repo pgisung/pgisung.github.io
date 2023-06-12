128b30a93b3b62ced282880a485844bac21c49c1
Johnny <pgisung@naver.com>
2023-06-12 23:35:02 +0900

    Web prototype pc version complete

--- custom.js
+++ custom.js
@@ -1,36 +1,46 @@
 // html 문서를 다 읽고난 후에 실행함
 $(function () {
     // Trigger
-    $('.trigger').click(function(){
+    $('.trigger').click(function () {
         $(this).toggleClass('active')
         $('.gnb').toggleClass('active')
     })
-    $('section, .menu a').click(function(){
+    $('section, .menu a').click(function () {
         $('.trigger').removeClass('active')
         $('.gnb').removeClass('active')
     })
 
     // Smooth Scrolling
-    $('.menu a, .gototop').click(function(e){
+    $('.menu a, .gototop').click(function (e) {
         $.scrollTo(this.hash || 0, 900)
     })
 
     // Change CSS with Scroll
-    $(window).scroll(function(){
-        if($(window).scrollTop() > 50){
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 50) {
             $('header, .gototop').addClass('active')
-        }
-        else{
+        } else {
             $('header, .gototop').removeClass('active')
         }
     })
 
-    // Slick.js : plan
+    // Slick.js : plan history
     $('.history-slider').slick({
         dots: true,
         infinite: false,
         speed: 300,
         slidesToShow: 4,
         slidesToScroll: 2,
-      });
+    });
+
+    // Slick.js : project photo
+    $('.project-photo').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        cssEase: 'linear'
+    });
 })
